ef83396f1c48a1df57ff6867e4ef57c4f6dfab49
pomelo1213 <goodmanluoxin@163.com>
2018-11-18 19:24:01 +0800

    update: 增加标记

--- main.js
+++ main.js
@@ -2,44 +2,42 @@ window.onload = function(){
   init()
 }
 
+var options = {
+  widht: 30,
+  height: 15,
+  title: "<b>这是介绍</b>",
+  enableMessage: true
+}
+
+var content = {
+  lake: "东湖我已经玩过了，不想玩了，偶尔去骑骑车",
+  temple: "黄鹤楼太贵了，没玩过orz，有钱在进去玩"
+}
+
 function init(){
   var map = new BMap.Map("map")
-  var point1 = new BMap.Point(114.404, 30.56645)
-  var point2 = new BMap.Point(114.314, 30.54445)
+  var point_lake = new BMap.Point(114.404, 30.56645)
+  var point_temple = new BMap.Point(114.314, 30.55445)
+  var marker_lake = new BMap.Marker(point_lake)
+  var marker_temple = new BMap.Marker(point_temple) 
   map.enableScrollWheelZoom(true)
   map.centerAndZoom("武汉", 12)
-  var marker1 = new BMap.Marker(point1)  
-  var marker2 = new BMap.Marker(point2)   
-  map.addOverlay(marker1)
-  map.addOverlay(marker2)
-  var content1 = "<div>东湖我已经玩过了，不想玩了，偶尔去骑骑车</div>"
-  var content2 = "<div>黄鹤楼太贵了，没玩过orz，有钱在进去玩</div>"
-  var options = {
-    widht: 30,
-    height: 15,
-    title: "<b>这是测试</b>",
-    enableMessage: true
-  }
+  map.addOverlay(marker_lake)
+  map.addOverlay(marker_temple)
 
-  // addListener('mouseover', )
+  EventHub(marker_lake, content.lake, options)
+  EventHub(marker_temple, content.temple, options)
+}
 
-  marker1.addEventListener("mouseover", function () {
-    this.openInfoWindow(new BMap.InfoWindow(content1, options))
-  });
-  marker1.addEventListener('mouseout', function(){
-    this.closeInfoWindow()
+function EventHub(marker, content, options) {
+  marker.addEventListener("mouseover", function () {
+    this.openInfoWindow(new BMap.InfoWindow(content, options))
   })
-  marker2.addEventListener("mouseover", function () {
-    this.openInfoWindow(new BMap.InfoWindow(content2, options))
-  });
-  marker2.addEventListener('mouseout', function () {
+  marker.addEventListener('mouseout', function () {
     this.closeInfoWindow()
   })
 }
 
 
-// function addListener(action, func, marker){
-//   marker.addEventListener(action, func)
-// }
 
 
